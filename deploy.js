5661f6631b4631f1a097b5efbc94432d4755c6be
Yesterday17 <t@yesterday17.cn>
2019-05-09 22:37:42 +0800

    ci skip

--- deploy.js
+++ deploy.js
@@ -12,7 +12,7 @@ ghpages.publish(
       process.env.GH_TOKEN
     }@github.com/mmf-moe/radiko-bangumi-json.git`,
     silent: true,
-    message: "Update triggered automatically.",
+    message: `Update triggered automatically.\n\nAt ${Date.now()}(${new Date()}) [ci skip]`,
     user: {
       name: "Yesterday17",
       email: "t@yesterday17.cn"
